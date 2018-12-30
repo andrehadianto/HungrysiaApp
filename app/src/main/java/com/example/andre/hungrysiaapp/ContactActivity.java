9d1680878844d8b36ee1439e5adc6178acb09519
cmetramexs <32505600+cmetramexs@users.noreply.github.com>
2018-12-30 14:42:26 +0800

    intent to contact activity

---
+++ app/src/main/java/com/example/andre/hungrysiaapp/ContactActivity.java
@@ -0,0 +1,6 @@
+package com.example.andre.hungrysiaapp;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class ContactActivity extends AppCompatActivity {
+}
--- app/src/main/java/com/example/andre/hungrysiaapp/MainActivity.java
+++ app/src/main/java/com/example/andre/hungrysiaapp/MainActivity.java
@@ -1,14 +1,18 @@
 //1  secret code
 package com.example.andre.hungrysiaapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView text_secretcode;
     private EditText edittext_secretcode;
+    private String string_secretcode = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,22 @@ public class MainActivity extends AppCompatActivity {
 
         text_secretcode = findViewById(R.id.secretcode_text);
         edittext_secretcode = findViewById(R.id.secretcode_edittext);
-    }
 
+        edittext_secretcode.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after){}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                string_secretcode = edittext_secretcode.getText().toString();
+                if (string_secretcode == getResources().getString(R.string.secret_code)){
+                    Intent toContact = new Intent(MainActivity.this,ContactActivity.class);
+                    startActivity(toContact);
+                }
+            }
+        });
+    }
 }
