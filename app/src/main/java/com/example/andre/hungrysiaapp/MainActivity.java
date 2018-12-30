321cff1dff7fba680f5c1f11bcde9cf6ecb2836b
cmetramexs <32505600+cmetramexs@users.noreply.github.com>
2018-12-30 14:13:52 +0800

    secret code page and theme

--- app/src/main/java/com/example/andre/hungrysiaapp/MainActivity.java
+++ app/src/main/java/com/example/andre/hungrysiaapp/MainActivity.java
@@ -1,13 +1,22 @@
+//1  secret code
 package com.example.andre.hungrysiaapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView text_secretcode;
+    private EditText edittext_secretcode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text_secretcode = findViewById(R.id.secretcode_text);
+        edittext_secretcode = findViewById(R.id.secretcode_edittext);
     }
+
 }
