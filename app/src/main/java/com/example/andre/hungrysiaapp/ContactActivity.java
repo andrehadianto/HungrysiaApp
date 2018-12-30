79a07758269502db84639ddfebca562fd371e9ca
cmetramexs <32505600+cmetramexs@users.noreply.github.com>
2018-12-30 16:23:43 +0800

    next buttons and implementing contact activity

--- app/src/main/java/com/example/andre/hungrysiaapp/ContactActivity.java
+++ app/src/main/java/com/example/andre/hungrysiaapp/ContactActivity.java
@@ -1,6 +1,54 @@
 package com.example.andre.hungrysiaapp;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ContactActivity extends AppCompatActivity {
+    private TextView text_name;
+    private EditText edittext_name;
+    private TextView text_phone;
+    private EditText editText_phone;
+    private TextView text_cohort;
+    private Spinner spinner_cohort;
+    private Button button_contact_next;
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact);
+
+        text_name = findViewById(R.id.name_text);
+        edittext_name = findViewById(R.id.name_edittext);
+        text_phone = findViewById(R.id.phone_text);
+        editText_phone = findViewById(R.id.phone_edittext);
+        text_cohort = findViewById(R.id.cohort_text);
+        spinner_cohort = findViewById(R.id.cohort_spinner);
+        button_contact_next = findViewById(R.id.contact_next_button);
+
+        final String[] cohorts_ = {"Select your cohort","F01","F02","F03","F04","F05","F06","F07","F08","F09","F10","ASD","EPD","ESD","ISTD"};
+        List<String> cohorts = new ArrayList<>(Arrays.asList(cohorts_));
+        ArrayAdapter<String> adapter_cohorts = new ArrayAdapter<String>(ContactActivity.this,android.R.layout.simple_spinner_item,cohorts);
+        adapter_cohorts.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner_cohort.setAdapter(adapter_cohorts);
+
+        button_contact_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String string_phone = text_phone.getText().toString();
+                if (string_phone.charAt(0)!=8 && string_phone.charAt(0)!=9){
+                    Toast.makeText(ContactActivity.this,"Invalid phone number",Toast.LENGTH_LONG);
+                }
+            }
+        });
+    }
 }
--- app/src/main/java/com/example/andre/hungrysiaapp/MainActivity.java
+++ app/src/main/java/com/example/andre/hungrysiaapp/MainActivity.java
@@ -6,13 +6,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private TextView text_secretcode;
     private EditText edittext_secretcode;
-    private String string_secretcode = "";
+    private Button button_secretcode_next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,20 +24,18 @@ public class MainActivity extends AppCompatActivity {
 
         text_secretcode = findViewById(R.id.secretcode_text);
         edittext_secretcode = findViewById(R.id.secretcode_edittext);
+        button_secretcode_next = findViewById(R.id.secretcode_next_button);
 
-        edittext_secretcode.addTextChangedListener(new TextWatcher() {
+        button_secretcode_next.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after){}
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {}
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                string_secretcode = edittext_secretcode.getText().toString();
+            public void onClick(View v) {
+                String string_secretcode = edittext_secretcode.getText().toString();
                 if (string_secretcode == getResources().getString(R.string.secret_code)){
                     Intent toContact = new Intent(MainActivity.this,ContactActivity.class);
                     startActivity(toContact);
+                }
+                else{
+                    Toast.makeText(MainActivity.this,R.string.hint,Toast.LENGTH_LONG).show();
                 }
             }
         });
