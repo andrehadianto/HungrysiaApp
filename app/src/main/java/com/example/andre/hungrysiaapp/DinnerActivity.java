64441955d186c68dacc3f3cdd706c03481c38d9d
andrehadianto <andre_hadianto@mymail.sutd.edu.sg>
2019-01-21 20:59:17 +0800

    Added more menu to dinner activity. Added SupperActivity and layout

--- app/src/main/java/com/example/andre/hungrysiaapp/DinnerActivity.java
+++ app/src/main/java/com/example/andre/hungrysiaapp/DinnerActivity.java
@@ -17,9 +17,20 @@ public class DinnerActivity extends AppCompatActivity {
         setContentView(R.layout.activity_dinner);
 
         dinnerMenu = new ArrayList<>();
-        dinnerMenu.add(new MenuItem("Salted Egg Chicken with Rice",4.50,R.drawable.salted_egg_chicken));
-        dinnerMenu.add(new MenuItem("Sweet and Sour Pork with Rice",5.00,R.drawable.sweet_and_sour_pork));
-        dinnerMenu.add(new MenuItem("Fried Bee Hoon",4.00,R.drawable.fried_bee_hoon));
+        dinnerMenu.add(new MenuItem("Mega Salted Egg Chicken Rice w Egg add Meat",7.00,R.drawable.mega_salted_egg_chicken));
+        dinnerMenu.add(new MenuItem("Salted Egg Chicken Rice w Egg",6.00,R.drawable.egg_salted_egg_chicken));
+        dinnerMenu.add(new MenuItem("Salted Egg Chicken  Rice",5.50,R.drawable.salted_egg_chicken));
+        dinnerMenu.add(new MenuItem("Black Butter Chicken Rice w Egg",6.00,R.drawable.egg_black_butter_chicken));
+        dinnerMenu.add(new MenuItem("Black Butter Chicken Rice",5.50,R.drawable.black_butter_chicken));
+        dinnerMenu.add(new MenuItem("Yang Chow Fried Rice w Egg",6.50,R.drawable.egg_yangzhou_fried_rice));
+        dinnerMenu.add(new MenuItem("Yang Chow Fried Rice ",5.50,R.drawable.yangzhou_fried_rice));
+        dinnerMenu.add(new MenuItem("Salted Veg Fried Meat Rice",5.50,R.drawable.asdf));
+        dinnerMenu.add(new MenuItem("Stewed Mixed Vegetable Rice",5.50,R.drawable.asdf));
+        dinnerMenu.add(new MenuItem("Crispy Noodles",5.50,R.drawable.asdf));
+        dinnerMenu.add(new MenuItem("Hongkong Mee",5.50,R.drawable.asdf));
+        dinnerMenu.add(new MenuItem("Beef Hor Fan",5.50,R.drawable.asdf));
+        dinnerMenu.add(new MenuItem("Fried Hor Fan (Vegetarian)",5.00,R.drawable.asdf));
+        dinnerMenu.add(new MenuItem("Mee Goreng (Vegetarian)",5.00,R.drawable.asdf));
 
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.dinner_recyclerview);
         RecyclerViewAdapter recyclerViewAdapter = new RecyclerViewAdapter(this,dinnerMenu);
--- app/src/main/java/com/example/andre/hungrysiaapp/SupperActivity.java
+++ app/src/main/java/com/example/andre/hungrysiaapp/SupperActivity.java
@@ -1,5 +1,43 @@
 package com.example.andre.hungrysiaapp;
 
-public class SupperActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SupperActivity extends AppCompatActivity{
+
+    List<MenuItem> supperMenu;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_supper);
+
+        supperMenu = new ArrayList<>();
+        supperMenu.add(new MenuItem("Mega Salted Egg Chicken Rice w Egg add Meat",7.00,R.drawable.mega_salted_egg_chicken));
+        supperMenu.add(new MenuItem("Salted Egg Chicken Rice w Egg",6.00,R.drawable.egg_salted_egg_chicken));
+        supperMenu.add(new MenuItem("Salted Egg Chicken  Rice",5.50,R.drawable.salted_egg_chicken));
+        supperMenu.add(new MenuItem("Black Butter Chicken Rice w Egg",6.00,R.drawable.egg_black_butter_chicken));
+        supperMenu.add(new MenuItem("Black Butter Chicken Rice",5.50,R.drawable.black_butter_chicken));
+        supperMenu.add(new MenuItem("Yang Chow Fried Rice w Egg",6.50,R.drawable.egg_yangzhou_fried_rice));
+        supperMenu.add(new MenuItem("Yang Chow Fried Rice ",5.50,R.drawable.yangzhou_fried_rice));
+        supperMenu.add(new MenuItem("Salted Veg Fried Meat Rice",5.50,R.drawable.asdf));
+        supperMenu.add(new MenuItem("Stewed Mixed Vegetable Rice",5.50,R.drawable.asdf));
+        supperMenu.add(new MenuItem("Crispy Noodles",5.50,R.drawable.asdf));
+        supperMenu.add(new MenuItem("Hongkong Mee",5.50,R.drawable.asdf));
+        supperMenu.add(new MenuItem("Beef Hor Fan",5.50,R.drawable.asdf));
+        supperMenu.add(new MenuItem("Fried Hor Fan (Vegetarian)",5.00,R.drawable.asdf));
+        supperMenu.add(new MenuItem("Mee Goreng (Vegetarian)",5.00,R.drawable.asdf));
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.supper_recyclerview);
+        RecyclerViewAdapter recyclerViewAdapter = new RecyclerViewAdapter(this,supperMenu);
+        recyclerView.setLayoutManager(new GridLayoutManager(this,2));
+        recyclerView.setAdapter(recyclerViewAdapter);
+    }
+
+
 
 }
